5aa001f8e5e9de0d7719492bd665d04134556b5e
mssimi <mssimi@seznam.cz>
2017-11-30 19:57:37 +0100

    init

---
+++ src/AppBundle/Controller/AppController.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AppController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction() : Response
+    {
+        return $this->render('app/index.html.twig', [
+        ]);
+    }
+}
---
+++ tests/AppBundle/Controller/AppControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AppControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
